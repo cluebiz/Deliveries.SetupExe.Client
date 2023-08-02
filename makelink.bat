54f895f5f5ff15d3fab217e9e6f40d855918ca53
Daniel Griner <daniel.griner@cluebiz.ch>
2023-08-02 17:58:07 +0200

    123

--- makelink.bat
+++ makelink.bat
@@ -1,8 +1,7 @@
-ECHO OFF
+mkdir code
+mklink /J "code/Deliveries.SetupExe" "../Deliveries.SetupExe"
+mklink /J "code/Deliveries.SetupExe.Logic" "../Deliveries.SetupExe.Logic"
+mklink /J "code/Deliveries.FastWrapper.ConvertEngine" "../Deliveries.FastWrapper.ConvertEngine"
+mklink /J "code/Deliveries.SetupExe.RunAsUserService" "../Deliveries.SetupExe.RunAsUserService"
 
-SET srcName=%~n1
-SET link=%~dp0%srcName%
 
-mklink /D %link% %~1
-
-REM set /p id="anykey "
