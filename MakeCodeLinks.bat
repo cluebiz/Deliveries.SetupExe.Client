abea924b1f65a2cb7b9d5ad4210695024d86135d
Daniel Griner <daniel.griner@cluebiz.ch>
2023-08-03 11:06:14 +0200

    batch commit

--- MakeCodeLinks.bat
+++
@@ -1,7 +0,0 @@
-mkdir code
-mklink /J "code/Deliveries.SetupExe" "../Deliveries.SetupExe"
-mklink /J "code/Deliveries.SetupExe.Logic" "../Deliveries.SetupExe.Logic"
-mklink /J "code/Deliveries.FastWrapper.ConvertEngine" "../Deliveries.FastWrapper.ConvertEngine"
-mklink /J "code/Deliveries.SetupExe.RunAsUserService" "../Deliveries.SetupExe.RunAsUserService"
-
-
